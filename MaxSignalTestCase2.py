248257b0cc47ee17b9f0f32e3b7e334b7b0426b1
AKHILAKOYADA <akhilakoyada22@gmail.com>
2026-05-22 14:50:18 +0530

    Update MaxSignalTestCase2.py

--- MaxSignalTestCase2.py
+++ MaxSignalTestCase2.py
@@ -1,1 +1,12 @@
-
+def maxStrength(n, a[], k):
+  if k==1:
+    return max(a)
+  if k==null or k==o:
+    return 0
+      
+   max_s = 0
+    for i in range(n):
+        sum=a[i:i+k]
+        if sum>max_s:
+            max_s=sum
+    return max_s
